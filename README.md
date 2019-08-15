cc2dce2cd376c0424cb7500da70beddbb7b4b148
Jonathan Medwig <jonmedwig@gmail.com>
2019-08-15 12:39:38 -0400

    Add binx script

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # binx
-Make a script executeable
+Make a script executeable system wide. Makes it easy to write small utility cli scripts and add them to /usr/local/bin
+
+## Install
+Binx can install itself!
+```
+$ git clone https://github.com/medwig/binx.git
+$ cd binx
+$ ./binx.sh binx.sh 
+
+Copied to /usr/local/bin as binx
+```
+
+## Usage
+After istalling just call binx and pass the path to the target script
+```
+$ binx path/to/myscript.py
+```
+
+## Notes
+Only supports python and bash scripts on linux right now.
---
+++ binx.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SCRIPT=$1		# Name of the script to make executeable
+BINDIR='/usr/local/bin' # The bin path to copy the script to
+
+# Exit if script argument is not given
+if [ -z "$1" ]
+  then
+    echo "Name of target script is a required argument"
+    exit 0
+fi
+
+# Exit if script argument is not a file
+if [ ! -f $SCRIPT ]; then
+    echo "File not found!"
+    exit 0
+fi
+
+# Create shebang based on filetype extension
+case "$SCRIPT" in
+    *.py) SHEBANG='#!/usr/bin/env python3' EXT='.py' ;;
+    *.sh) SHEBANG='#!/bin/bash' EXT='.sh' ;;
+esac
+
+# Copy script to /tmp for editing as $FILE
+cp $SCRIPT /tmp
+FILE="/tmp/$SCRIPT"
+
+# Add shebang if not present
+if [ "`head -c 2 $FILE`" != "#!" ]; then
+    echo "Adding shebang: $SHEBANG"
+    sed -i "1i$SHEBANG" $FILE
+fi
+
+# Make executeable
+chmod +x $FILE
+
+# Copy to bin dir
+EXNAME=$(basename $SCRIPT $EXT)  # trim file extension
+sudo cp $FILE $BINDIR/$EXNAME
+echo "Copied to $BINDIR as $EXNAME"
